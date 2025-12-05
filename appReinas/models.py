497abc7d108b521fde0a848285f684c1d75537fe
wilkin547 <wilkintrinidad10@gmail.com>
2025-12-04 20:45:43 -0400

    los torneos fueron conectados

--- appReinas/models.py
+++ appReinas/models.py
@@ -48,9 +48,17 @@ class Directivo(models.Model):
 class Torneo(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
-    fecha = models.DateField()
+    fecha = models.DateField(null=True, blank=True)
+    fechaend = models.DateField(null=True, blank=True)
     imagen = models.ImageField(upload_to='torneos/', null=True, blank=True)
-
+    categoria = models.CharField(max_length=100, blank=True, null=True)
+    pos1 = models.CharField(max_length=100, blank=True, null=True)
+    pos2 = models.CharField(max_length=100, blank=True, null=True)
+    pos3 = models.CharField(max_length=100, blank=True, null=True)
+    ultimocampeon = models.CharField(max_length=100, blank=True, null=True)
+    formato = models.TextField(blank=True, null=True)
+    equipos = models.TextField(blank=True, null=True)
+    sedes = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.titulo
 
